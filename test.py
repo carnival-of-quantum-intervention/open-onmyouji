de59fb992eb8033f5f99f8437b0ef429ac499a62
博奕 黄 <1853308@tongji.edu.cn>
2021-02-23 23:11:07 +0800

    Update test.py

--- test.py
+++ test.py
@@ -2,18 +2,23 @@ import cv2
 import numpy as np
 from window import *
 from picture import *
+from find_pic import *
 
 HWND = getHandle("阴阳师-网易游戏")
 # 读取图片
 captured = captureWindowAs(HWND, "cache/cache.png")
 
 src = captured
-cv2.namedWindow("input", cv2.WINDOW_AUTOSIZE)
-cv2.imshow("input", src)
 """
 提取图中的黄色部分
 """
-cv2.imshow(filterColor("test",
-                       np.array([3, 220, 220]), np.array([50, 255, 255])))
+filtered = filterColor(src,
+                       np.array([3, 220, 220]), np.array([50, 255, 255]))
+cv2.imshow("mask", filtered)
+print(find_full(filtered, cv2.cvtColor(
+    cv2.imread("picture/full.png"), cv2.COLOR_BGR2GRAY)))
 cv2.waitKey(0)
 cv2.destroyAllWindows()
+# cv2.imshow()
+# cv2.waitKey(0)
+# cv2.destroyAllWindows()
