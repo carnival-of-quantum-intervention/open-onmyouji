816a4f14f342c4cd17e8997156a7dd377cd37cd3
博奕 黄 <1853308@tongji.edu.cn>
2021-02-23 11:13:14 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,44 +1,15 @@
 # 加载环境包
 import json
+from os import stat
 from window import *
 from picture import *
+import threading
 import keyboard
 import time
-
-configs = json.load(open("config.json", 'r', encoding="utf-8"))
-print(configs)
-index = int(input("Input the index of the configure you'd like to use."))
-HWND = getHandle(configs["title"])
-if HWND == 0:
-    print("Can't find onmyouji.")
-    exit(1)
-
-config = configs["config"][index]
-
-ScreenWidth, ScreenHeight = getScreenSize()
-
-global left, top, right, bottom
-left, top, right, bottom = getRect(HWND)
-WindowWidth = right - left
-
-global on
-on = True
+import tkinter
 
 
-def watchEsc(Event):
-    if Event.event_type == 'down':
-        if Event.name == 'esc':
-            global on
-            on = False
-        if Event.name == 'f5':
-            global left, top, right, bottom, WindowWidth
-            left, top, right, bottom = getRect(HWND)
-            WindowWidth = right - left
-
-
-keyboard.hook(watchEsc)
-while(on):
-
+def work(config):
     captureWindowAs(HWND, "cache/cache.png")
     img = cv2.imread('cache/cache.png')  # 读取图片
     r = int(config["ratio"] * WindowWidth)
@@ -55,3 +26,78 @@ while(on):
         click(X, Y)
 
     time.sleep(1)
+
+
+configs = json.load(open("config.json", 'r', encoding="utf-8"))
+configList = [item["name"] for item in configs["config"]]
+
+HWND = getHandle(configs["title"])
+if HWND == 0:
+    print("Can't find onmyouji.")
+    exit(1)
+
+
+global left, top, right, bottom
+left, top, right, bottom = getRect(HWND)
+WindowWidth = right - left
+
+global on
+on = True
+
+
+def watchEsc(Event):
+    if Event.event_type == 'down':
+        if Event.name == 'esc':
+            global on
+            on = False
+        if Event.name == 'f5':
+            global left, top, right, bottom, windowWidth
+            left, top, right, bottom = getRect(HWND)
+            windowWidth = right - left
+
+
+keyboard.hook(watchEsc)
+
+ROOT = tkinter.Tk()
+
+global stateStr
+stateStr = tkinter.StringVar()
+stateStr.set("Waiting")
+state = tkinter.Label(ROOT, textvariable=stateStr)
+state.pack()
+
+
+class thread(threading.Thread):
+    def __init__(self, index):
+        self.__index = index
+        threading.Thread.__init__(self)
+
+    def set(self, index):
+        self.__index = index
+
+    def run(self):
+        global stateStr
+        while on:
+            if self.__index >= 0:
+                stateStr.set("Working")
+                work(configs["config"][self.__index])
+        stateStr.set("Dead")
+
+
+global mainThread
+mainThread = thread(-1)
+mainThread.start()
+lb = tkinter. Listbox(ROOT)
+
+
+def CallOn(event):
+    global mainThread
+    print(mainThread)
+    mainThread.set(lb.curselection()[0])
+
+
+# 双击命令
+lb.bind('<Double-Button-1>', CallOn)
+lb.insert(tkinter.END, *configList)
+lb.pack()
+ROOT.mainloop()
