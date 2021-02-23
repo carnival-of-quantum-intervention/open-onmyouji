cb6c8d31d56f200aa73595908f24921fa1c91e42
博奕 黄 <1853308@tongji.edu.cn>
2021-02-23 16:59:23 +0800

    Update window.py

--- window.py
+++ window.py
@@ -23,48 +23,24 @@ def click(x, y):
 
 def getRect(hwnd):
     import win32gui
+    import win32api
+    import win32con
     # 获取窗口左上角和右下角坐标
     wLeft, wTop, wRight, wBottom = win32gui.GetWindowRect(hwnd)
     cLeft, cTop, cRight, cBottom = win32gui.GetClientRect(hwnd)
-    return wLeft+cLeft, wTop+cTop, wLeft+cRight, wTop+cBottom
+    hTitle = win32api.GetSystemMetrics(win32con.SM_CYSIZE)
+    return wLeft + cLeft, hTitle + wTop + cTop, wLeft + cRight, wTop + cBottom + hTitle
 
 
 def captureWindowAs(hwnd, filename):
-    """ hwnd为窗口的编号，0号表示当前活跃窗口"""
-    import win32gui
-    import win32ui
-    import win32con
-    import win32api
-    # 根据窗口句柄获取窗口的设备上下文DC（Divice Context）
-    hwndDC = win32gui.GetWindowDC(hwnd)
-    # 根据窗口的DC获取mfcDC
-    mfcDC = win32ui.CreateDCFromHandle(hwndDC)
-    # mfcDC创建可兼容的DC
-    saveDC = mfcDC.CreateCompatibleDC()
-    # 创建bigmap准备保存图片
-    saveBitMap = win32ui.CreateBitmap()
-    # 获取监控器信息
-    MoniterDev = win32api.EnumDisplayMonitors(None, None)
-    w = MoniterDev[0][2][2]
-    h = MoniterDev[0][2][3]
-    # print w,h　　　#图片大小
-    # 为bitmap开辟空间
-    saveBitMap.CreateCompatibleBitmap(mfcDC, w, h)
-    # 高度saveDC，将截图保存到saveBitmap中
-    saveDC.SelectObject(saveBitMap)
-    # 截取从左上角（0，0）长宽为（w，h）的图片
-    saveDC.BitBlt((0, 0), (w, h), mfcDC, (0, 0), win32con.SRCCOPY)
-
-    saveBitMap.SaveBitmapFile(saveDC, filename)
-
+    from PIL import ImageGrab
+    import numpy as np
     import cv2
-    import numpy
-    import PIL.Image
-    bmpinfo = saveBitMap.GetInfo()
-    bmparray = numpy.asarray(saveBitMap.GetBitmapBits(), dtype=numpy.uint8)
-    pil_im = PIL.Image.frombuffer(
-        'RGB', (bmpinfo['bmWidth'], bmpinfo['bmHeight']), bmparray, 'raw', 'BGRX', 0, 1)
-    pil_array = numpy.array(pil_im)
-    cv_im = cv2.cvtColor(pil_array, cv2.COLOR_RGB2BGR)
-    # cv2.imwrite("cache/buf.png", cv_im)
-    return cv_im
+
+    img = ImageGrab.grab()
+
+    # PIL image to OpenCV image
+    im = np.array(img)
+    im = cv2.cvtColor(im, cv2.COLOR_RGB2BGR)
+    left, top, right, bottom = getRect(hwnd)
+    return im[top:bottom, left:right]
