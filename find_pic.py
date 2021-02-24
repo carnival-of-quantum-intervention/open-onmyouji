f55d667b3d9a2f3dd011f0f0d2f096240d2c82a1
博奕 黄 <1853308@tongji.edu.cn>
2021-02-24 09:05:24 +0800

    Update find_pic.py

--- find_pic.py
+++ find_pic.py
@@ -1,6 +1,26 @@
 
 
 def find_full(bg_img, expect_img):
+    import cv2
+    # 检测连通域，每一个连通域以一系列的点表示，FindContours方法只能得到第一个域
+    contours, hierarchy = cv2.findContours(
+        bg_img, method=cv2.CHAIN_APPROX_SIMPLE, mode=cv2.RETR_EXTERNAL)
+    color = (0, 0, 255)
+
+    rects = []
+    # 开始遍历
+    if contours != None:
+        for contour in contours:
+            # 得到这个连通区域的外接矩形
+            x, y, w, h = cv2.boundingRect(contour)
+
+            # 如果高度不足，或者长宽比太小，认为是无效数据，否则把矩形画到原图上
+            if h > 10 and (w * 1.0 / h) > 0.2:
+                cv2.rectangle(src, (x, y), (x+w, y+h), color)
+                rects.append((x, y, w, h))
+    cv2.imshow("test", src)
+    return rects
+
     extend_point0 = set()
     extend_point = []
     count = 0
@@ -48,10 +68,16 @@ if __name__ == '__main__':
     import numpy as np
     import picture
     import cv2
-    p1 = cv2.imread("picture/test.png")
+    src = cv2.imread("picture/test.png")
     p2 = cv2.imread("picture/full.png")
-    p1 = picture.filterColor(p1, np.array(
-        [3, 220, 220]), np.array([50, 255, 255]))
+    p1 = picture.filterColor(src, np.array(
+        [10, 210, 0]), np.array([40, 255, 255]))
     p2 = cv2.cvtColor(p2, cv2.COLOR_BGR2GRAY)
     res = find_full(p1, p2)
+    for i in res:
+        cv2.circle(src, (i[1], i[0]), 1, (0, 0, 255))
+    cv2.imshow("Picture", src)
+    cv2.imshow("Processed picture", p1)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
     print(res)
