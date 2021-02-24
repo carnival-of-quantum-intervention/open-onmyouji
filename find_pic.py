99ced0ae758474daf07ee9e22a30e3753c365c2a
博奕 黄 <1853308@tongji.edu.cn>
2021-02-24 08:07:43 +0800

    Update find_pic.py

--- find_pic.py
+++ find_pic.py
@@ -1,31 +1,32 @@
 import cv2
 
-def find_full(bg_img,expect_img):
+
+def find_full(bg_img, expect_img):
     extend_point0 = set()
-    extend_point = []    
-    count= 0
+    extend_point = []
+    count = 0
     for i in range(expect_img.shape[0]):
         for j in range(expect_img.shape[1]):
             if expect_img[i][j] != 0:
-                count+=1
+                count += 1
     for i in range(bg_img.shape[0]-expect_img.shape[0]):
-        if i < 0.15 * bg_img.shape[0] or i > 0.7 *bg_img.shape[0]:
+        if i < 0.15 * bg_img.shape[0] or i > 0.7 * bg_img.shape[0]:
             continue
         for j in range(bg_img.shape[1]-expect_img.shape[1]):
-            if j < 0.15 * bg_img.shape[1] or j > 0.85 *bg_img.shape[1]:
+            if j < 0.15 * bg_img.shape[1] or j > 0.85 * bg_img.shape[1]:
                 continue
             if bg_img[i][j] > 0:
-                for I in range(-expect_img.shape[0]+1,expect_img.shape[0]):
+                for I in range(-expect_img.shape[0]+1, expect_img.shape[0]):
                     if i+I < 0 or i+I >= bg_img.shape[0]:
                         continue
-                    for J in range(-expect_img.shape[1]+1,expect_img.shape[1]):
+                    for J in range(-expect_img.shape[1]+1, expect_img.shape[1]):
                         if j+J < 0 or j+J >= bg_img.shape[1]:
                             continue
                         extend_point0.add((i+I)*bg_img.shape[1]+j+J)
     res = []
     pic_size = expect_img.shape[0] * expect_img.shape[1]
     for p in extend_point0:
-        extend_point.append([int(p/bg_img.shape[1]),p%bg_img.shape[1]])
+        extend_point.append([int(p/bg_img.shape[1]), p % bg_img.shape[1]])
     for p in extend_point:
         similarity = 0
         zeronum = 0
@@ -39,12 +40,14 @@ def find_full(bg_img,expect_img):
                         i = expect_img.shape[0] + 1
                         zeronum = -1
                         break
-        if zeronum != -1 and similarity/count > 0.5: #0.5是我自己定的阈值，即相似度大于50%就选择
-            res.append([p[0],p[1],similarity/count])
+        if zeronum != -1 and similarity/count > 0.5:  # 0.5是我自己定的阈值，即相似度大于50%就选择
+            res.append([p[0], p[1], similarity/count])
     return res
-                
-p1 = cv2.imread("test.png")
-p2 = cv2.imread("full.png")
-p1 = cv2.cvtColor(p1,cv2.COLOR_BGR2GRAY)
-p2 = cv2.cvtColor(p2,cv2.COLOR_BGR2GRAY)
-res = find_full(p1, p2)
+
+
+if __name__ == '__main__':
+    p1 = cv2.imread("test.png")
+    p2 = cv2.imread("full.png")
+    p1 = cv2.cvtColor(p1, cv2.COLOR_BGR2GRAY)
+    p2 = cv2.cvtColor(p2, cv2.COLOR_BGR2GRAY)
+    res = find_full(p1, p2)
